9d51e7a2df95b354de304aec3fc2fc331316fb54
tylersisia <tyler@pdssystems.com>
2019-11-15 14:35:08 -0500

    Update config.js

--- website/config.js
+++ website/config.js
@@ -1,8 +1,8 @@
-var api = 'http://poolhost.com:8117'
+var api = 'http://chimera.pdss2access.com:8117'
 
-var api_blockexplorer = 'http://daemonhost.com:11898'
+var api_blockexplorer = 'http://chimera.pdss2access.com:11246'
 
-var poolHost = 'poolhost.com'
+var poolHost = 'chimera.pdss2access.com'
 
 var irc = 'irc.freenode.net/#poolhost'
 
@@ -19,43 +19,9 @@ var transactionExplorer = 'http://chainradar.com/{symbol}/transaction/{id}'
 var themeCss = 'themes/default-theme.css'
 
 var networkStat = {
-  'bcn': [
-    ['bcn.mypool.online', 'http://bcn.mypool.online:8084'],
-    ['democats.org', 'http://pool.democats.org:7603']
-  ],
-  'bip': [
-    ['bip.mypool.online', 'http://bip.mypool.online:18874'],
-    ['democats.org', 'http://pool.democats.org:7693'],
-    ['bip.cryptonotepool.com', 'http://5.189.135.137:8121'],
-    ['bip.ms-pool.net.ua', 'http://bip.ms-pool.net.ua:8117'],
-    ['bip.crypto-coins.club', 'http://bip.crypto-coins.club:8118']
-  ],
-  'coal': [
-    ['coal.mypool.online', 'http://coal.mypool.online:7704'],
-    ['democats.org', 'http://pool.democats.org:7703']
-  ],
-  'dsh': [
-    ['dsh.mypool.online', 'http://dsh.mypool.online:29084'],
-    ['democats.org', 'http://pool.democats.org:7613']
-  ],
-  'fcn': [
-    ['fcn.mypool.online', 'http://fcn.mypool.online:24084']
-  ],
-  'krb': [
-    ['krb.mypool.online', 'http://krb.mypool.online:32351'],
-    ['democats.org', 'http://pool2.democats.org:7673'],
-    ['pool.karbowanec.com', 'http://pool.karbowanec.com:8117'],
-    ['pool2.karbowanec.com', 'http://pool2.karbowanec.com:8117'],
-    ['krb.sberex.com', 'http://krb.sberex.com:7006'],
-    ['krb.crypto-coins.club', 'http://krb.crypto-coins.club:8118'],
-    ['krb.cryptonotepool.com', 'http://5.189.135.137:8618'],
-    ['krbpool.ml', 'http://krbpool.ml:8117']
-  ],
-  'qcn': [
-    ['qcn.mypool.online', 'http://qcn.mypool.online:23084']
-  ],
-  'xci': [
-    ['xci.mypool.online', 'http://xci.mypool.online:42004'],
-    ['xci.cryptonotepool.com', 'http://5.189.135.137:8119']
+'cmra': [
+    ['chimera.pdss2access.com', 'http://chimera.pdss2access.com:8117'],
+    ['cmra.miningpool.fun', 'https://cmra.miningpool.fun:8151'],
+    ['54.172.240.25', 'http://54.172.240.25:8117']
   ]
 }
